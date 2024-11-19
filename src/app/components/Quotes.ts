118aa6d29ad871208f02c7fe564472a012430398
mahari9 <merytseg77@gmail.com>
2024-11-19 20:19:57 +0300

    Add ComboQuotes component to provide a collection of inspirational quotes

---
+++ src/app/components/Quotes.ts
@@ -0,0 +1,318 @@
+interface ComboQuote {
+    id: number;
+    category: string;
+    quote: string;
+    author: string;
+  }
+  
+  const ComboQuotes: ComboQuote[] = [
+    {
+      id: 1,
+      category: "Life Quote",
+      quote: "Even if things go south, you will survive and so will the world”",
+      author: "Unknown Author",
+    },
+    {
+      id: 2,
+      category: "Nature Quote",
+      quote: "Look deep into nature, and then you will understand everything better.",
+      author: "Albert Einstein",
+    },
+    {
+      id: 3,
+      category: "Developers Quote",
+      quote: "It's not a bug, it's a feature.",
+      author: "Unknown Author",
+    },
+    {
+      id: 4,
+      category: "Life Quote",
+      quote: "You are not too young, you are not too old, you are not too unprepared. You have a plan, and that’s enough",
+      author: "Unknown Author",
+    },
+    {
+      id: 5,
+      category: "Nature Quote",
+      quote: "The poetry of the earth is never dead.",
+      author: "John Keats",
+    },
+    {
+      id: 6,
+      category: "Developers Quote",
+      quote: "Hello World! You can't trust the internet",
+      author: "Unknown Author & Nicollette Sheridan",
+    },
+    {
+      id: 7,
+      category: "Fun Quote",
+      quote: "We are all here on earth to help others; what on earth the others are here for I don't know!",
+      author: "W. H. Auden",
+    },
+    {
+      id: 8,
+      category: "Imagination Quotes",
+      quote: "Logic will get you from A to B. Imagination will take you everywhere.",
+      author: "Albert Einstein",
+    },
+    {
+      id: 9,
+      category: "Nature Quote",
+      quote: "Adopt the pace of nature: her secret is patience.",
+      author: "Ralph Waldo Emerson",
+    },
+    {
+      id: 10,
+      category: "Developers Quote",
+      quote: "The best error message is the one that never shows up.",
+      author: "Thomas Fuchs",
+    },
+    {
+      id: 11,
+      category: "Positive Quotes",
+      quote: "Work hard, stay positive, and get up early. It's the best part of the day.",
+      author: "George Allen, Sr",
+    },
+    {
+      id: 12,
+      category: "Knowledge Quotes",
+      quote: "To know, is to know that you know nothing. That is the meaning of true knowledge.",
+      author: "Socrates",
+    },
+    {
+      id: 13,
+      category: "Developers Quote",
+      quote: "Why do programmers prefer dark mode? Because light attracts bugs!",
+      author: "Unknown Author",
+    },
+    {
+      id: 14,
+      category: "Life Quote",
+      quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+      author: "Albert Einstein",
+    },
+    {
+      id: 15,
+      category: "Nature Quote",
+      quote: "Study nature, love nature, stay close to nature. It will never fail you.",
+      author: "Frank Lloyd Wright",
+    },
+    {
+      id: 16,
+      category: "Developers Quote",
+      quote: "Code is like humor. When you have to explain it, it’s bad.",
+      author: "Cory House",
+    },
+    {
+      id: 17,
+      category: "Knowledge Quotes",
+      quote: "Those who have knowledge, don't predict. Those who predict, don't have knowledge.",
+      author: "Lao Tzu",
+    },
+    {
+      id: 18,
+      category: "Life Quote",
+      quote: "Life is a journey that must be traveled no matter how bad the roads and accommodations.",
+      author: "Oliver Goldsmith",
+    },
+    {
+      id: 19,
+      category: "Alone Quote",
+      quote: "By all means use some time to be alone",
+      author: "Edward Young",
+    },
+    {
+      id: 20,
+      category: "Amazing Quote",
+      quote: "It's such an amazing thing to be loved for who you are.",
+      author: "Debra Winger",
+    },
+    {
+      id: 21,
+      category: "Life Quote",
+      quote: "In a world where people sit and watch people fall, be a person who helps people climb.",
+      author: "Rachel Mitchell",
+    },
+    {
+      id: 22,
+      category: "Development Quote",
+      quote: "If at first you didn't succeed, call it version 1.0.",
+      author: "Caelan Huntress",
+    },
+    {
+      id: 23,
+      category: "Attitude Quote",
+      quote: "Attitude is a little thing that makes a big difference.",
+      author: "Winston Churchill",
+    },
+    {
+      id: 24,
+      category: "Change Quote",
+      quote: "If you don't like something, change it. If you can't change it, change your attitude.",
+      author: "Unknown",
+    },
+    {
+      id: 25,
+      category: "Business Quote",
+      quote: "If you want to kill any idea in the world, get a committee working on it.",
+      author: "Charles Kettering",
+    },
+    {
+      id: 26,
+      category: "Technology Quote",
+      quote: "The best way to predict the future is to invent it.",
+      author: "Alan Kay",
+    },
+    {
+      id: 27,
+      category: "Courage Quote",
+      quote: "The secret to happiness is freedom... And the secret to freedom is courage.",
+      author: "Thucydides",
+    },
+    {
+      id: 28,
+      category: "Development Quote",
+      quote: "The best way to predict the future is to create it.",
+      author: "Peter Drucker",
+    },
+    {
+      id: 29,
+      category: "Communication Quote",
+      quote: "Words are, of course, the most powerful drug used by mankind.",
+      author: "Rudyard Kipling",
+    },
+    {
+      id: 30,
+      category: "Brainy Quote",
+      quote: "The time is always right to do what is right.",
+      author: "Martin Luther King, Jr.",
+    },
+    {
+      id: 31,
+      category: "Brainy Quote",
+      quote: "We don't see things as they are, we see them as we are.",
+      author: "Anais Nin",
+    },
+    {
+      id: 32,
+      category: "Development Quote",
+      quote: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs",
+    },
+    {
+      id: 33,
+      category: "Brainy Quote",
+      quote: "Fortune favors the prepared mind.",
+      author: "Louis Pasteur",
+    },
+    {
+      id: 34,
+      category: "Computers Quote",
+      quote: "To err is human, but to really foul things up you need a computer.",
+      author: " Paul R. Ehrlich",
+    },
+    {
+      id: 35,
+      category: "Career Quote",
+      quote: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs",
+    },
+    {
+      id: 36,
+      category: "Chance Quote",
+      quote: "If you are not willing to risk the unusual, you will have to settle for the ordinary.",
+      author: "Jim Rohn",
+    },
+    {
+      id: 37,
+      category: "Dating Quote",
+      quote: "You can't keep changing men, so you settle for changing your lipstick.",
+      author: "Heather Locklear",
+    },
+    {
+      id: 38,
+      category: "Education Quote",
+      quote: "Education is the most powerful weapon which you can use to change the world.",
+      author: "Nelson Mandela",
+    },
+    {
+      id: 39,
+      category: "Education Quote",
+      quote: "I have no special talent. I am only passionately curious.",
+      author: "Albert Einstein",
+    },
+    {
+      id: 40,
+      category: "Environmental Quote",
+      quote: "Try to leave the Earth a better place than when you arrived.",
+      author: "Sidney Sheldon",
+    },
+    {
+      id: 41,
+      category: "Equality Quote",
+      quote: "The idea of equal rights was in the air.",
+      author: "Lucy Stone",
+    },
+    {
+      id: 42,
+      category: "Experience Quote",
+      quote: "Be brave. Take risks. Nothing can substitute experience.",
+      author: "Paulo Coelho",
+    },
+    {
+      id: 43,
+      category: "Failure Quote",
+      quote: "I have not failed. I've just found 10,000 ways that won't work.",
+      author: "Thomas A. Edison",
+    },
+    {
+      id: 44,
+      category: "Development Quote",
+      quote: "The expert in anything was once a beginner.",
+      author: "Helen Hayes",
+    },
+    {
+      id: 45,
+      category: "Failure Quote",
+      quote: "Do not blame anybody for your mistakes and failures.",
+      author: "Bernard Baruch",
+    },
+    {
+      id: 46,
+      category: "Faith Quote",
+      quote: "We are twice armed if we fight with faith.",
+      author: "Plato",
+    },
+    {
+      id: 47,
+      category: "Fear Quote",
+      quote: "The only thing we have to fear is fear itself.",
+      author: "Franklin D. Roosevelt",
+    },
+    {
+      id: 48,
+      category: "Freedom Quote",
+      quote: "Freedom is the right to tell people what they do not want to hear.",
+      author: "George Orwell",
+    },
+    {
+      id: 49,
+      category: "Politics Quote",
+      quote: "We would all like to vote for the best man but he is never a candidate.",
+      author: "Kin Hubbard",
+    },
+    {
+      id: 50,
+      category: "Technology Quote",
+      quote: "What new technology does is create new opportunities to do a job that customers want done.",
+      author: "Tim O'Reilly",
+    },
+    {
+      id: 51,
+      category: "Nature Quote",
+      quote: "Nothing makes me so happy as to observe nature.",
+      author: "Mahari Tsegay",
+    },
+  ];
+  
+  export default ComboQuotes;
+  
