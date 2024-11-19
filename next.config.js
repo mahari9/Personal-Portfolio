747406eac01ea9e50e4ab23eb5ac59f0b19cadd4
mahari9 <merytseg77@gmail.com>
2024-11-19 19:47:03 +0300

    Initialize Next.js project with essential configuration files and dependencies

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+module.exports = {
+    publicRuntimeConfig: {
+      metadataBase: 'https://your-home-slai.onrender.com',
+    },
+  };
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        jet: '#333333',
+        'smoky-gray': '#383838',
+        'gray': '#9f9f9f',
+        white: '#FFFFFF',
+        litewhite: '#FAFAFA',
+      },
+      backgroundImage: {
+        'smoky-gray': '#39393a',
+      },
+      fontWeight: {
+        '100': '100',
+        '200': '200',
+        '300': '300',
+        '400': '400',
+        '500': '500',
+        '600': '600',
+        '700': '700',
+        '800': '800',
+        '900': '900',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
