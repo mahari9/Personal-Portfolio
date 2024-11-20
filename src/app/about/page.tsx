339ef0917eadd86de2211d829146b0ecda0bdd64
mahari9 <merytseg77@gmail.com>
2024-11-20 11:29:15 +0300

    Remove unused metadata and clean up About page component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,49 +1,8 @@
 'use client'
-import type { Metadata } from 'next';
 import WhatIDo from "../components/WhatIDo";
 import Skills from "../components/Skills";
 import InscribeQuotes from "../components/InscribeQuotes";
-import BackToTop from "../components/BackToTop"
-
-export const metadata: Metadata = {
-    title: 'Mahari Tsegay Portfolio',
-    description: 'Personal portfolio website developed using React.js and Next.js',
-    openGraph: {
-      title: 'Mahari Tsegay Portfolio',
-      description: 'Personal portfolio website developed using React.js and Next.js',
-      url: 'https://mahari-slai.onrender.com',
-      images: ['https://github.com/Lordwill1/my_portfolio/assets/105258746/fc32b316-0838-432c-9e66-f8279c62bc6b'],
-      type: 'website',
-    },
-    icons: {
-      icon: '/favicon_io/favicon.ico',
-      shortcut: '/favicon_io/favicon.ico',
-      apple: '/favicon_io/apple-touch-icon.png',
-      other: {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/apple-touch-icon-precomposed.png',
-      },
-    },
-  
-    manifest: 'https://mahari-slai.render.com/manifest.json',
-  
-    twitter: {
-      card: 'summary_large_image',
-      title: 'Mahari Tsegay | Portfolio',
-      description: 'Personal portfolio website developed using React.js and Next.js',
-      images: ['https://nextjs.org/og.png'],
-    },
-  
-    other: {
-      'google-site-verification': 'verification_token',
-      viewport: 'width=device-width, initial-scale=1',
-      'itemprop:name': 'Mahari Tsegay | Portfolio',
-      'itemprop:description': 'Personal portfolio website developed using React.js and Next.js',
-      'itemprop:image': 'https://github.com/Lordwill1/my_portfolio/assets/105258746/fc32b316-0838-432c-9e66-f8279c62bc6b',
-      robots: 'index, follow',
-    },
-  };
-
+import BackToTop from "../components/BackToTop";
 export default function Home() {
     return (
         <main className="flex w-full text-white bg-gradient-to-tr from-stone-950 via-green-950 to-stone-950 p-[5%] flex flex-col gap-10 mt-0 mb-0 overflow-hidden">
